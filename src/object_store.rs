45b35a5cbcdfb7d43e0e65ac7b1963c91468ded6
Léo Gaspard <leo@gaspard.io>
2024-01-13 23:13:06 +0100

    factor out add error handling

--- src/object_store.rs
+++ src/object_store.rs
@@ -23,20 +23,22 @@ impl<Err> ObjectStore<Err> {
     ///
     /// Internally, this uses [`IDBObjectStore::add`](https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore/add).
     pub async fn add(&self, value: &JsValue) -> Result<JsValue, crate::Error<Err>> {
-        let add_req = self.sys.add(value).map_err(|err| {
-            match error_name!(&err) {
-                Some("ReadOnlyError") => crate::Error::ReadOnly,
-                Some("TransactionInactiveError") => {
-                    panic!("Tried adding to an ObjectStore while the transaction was inactive")
-                }
-                Some("DataError") => crate::Error::InvalidKey,
-                Some("InvalidStateError") => crate::Error::ObjectStoreWasRemoved,
-                Some("DataCloneError") => crate::Error::FailedClone,
-                Some("ConstraintError") => crate::Error::AlreadyExists,
-                _ => crate::Error::from_js_value(err),
-            }
-            .into_user()
-        })?;
+        let add_req = self.sys.add(value).map_err(map_add_err)?;
         transaction_request::<Err>(add_req).await
     }
+}
+
+fn map_add_err<Err>(err: JsValue) -> crate::Error<Err> {
+    match error_name!(&err) {
+        Some("ReadOnlyError") => crate::Error::ReadOnly,
+        Some("TransactionInactiveError") => {
+            panic!("Tried adding to an ObjectStore while the transaction was inactive")
+        }
+        Some("DataError") => crate::Error::InvalidKey,
+        Some("InvalidStateError") => crate::Error::ObjectStoreWasRemoved,
+        Some("DataCloneError") => crate::Error::FailedClone,
+        Some("ConstraintError") => crate::Error::AlreadyExists,
+        _ => crate::Error::from_js_value(err),
+    }
+    .into_user()
 }
